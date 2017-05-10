50012f548b84576d78633d9299f1dbae7eac3192
DaniloLopez <dlopezs@unicauca.edu.co>
2017-05-09 22:26:53 -0500

    :mouse: cambio TS del componente que recibe el max y minimo de un sensor

--- src/components/rango-sensor/rango-sensor.ts
+++ src/components/rango-sensor/rango-sensor.ts
@@ -1,12 +1,12 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'rango-sensor',
   templateUrl: 'rango-sensor.html'
 })
 export class RangoSensor implements OnInit {
-  //@Output() max: string;
-  //@Output() min: string;  
+  @Output() max: string;
+  @Output() min: string;  
 
 //es recomendable que en el contructor solo se declaren variables  
   constructor() {
